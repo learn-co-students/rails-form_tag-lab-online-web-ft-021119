041f234dc9a462113c337223ab7674cf385b3706
BebopVinh <vinhdo07@gmail.com>
2019-04-16 21:19:33 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.new(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    if @student.save
+      redirect_to student_path(@student.id)
+    else
+      redirect_to new_student_path
+    end
   end
-
 end
